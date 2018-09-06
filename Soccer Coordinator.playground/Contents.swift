04e940f9a308811e81929fe0363a0aabd38225e3
Michael Hulet <michael@hulet.tech>
2018-09-06 01:34:38 -0500

    Sorted players into teams based on the extra credit specification

--- Soccer Coordinator.playground/Contents.swift
+++ Soccer Coordinator.playground/Contents.swift
@@ -14,14 +14,14 @@
  - ✅ Store players for each team in collections named `teamSharks`, `teamDragons`, and `teamRaptors`
 
  #### Part 2: Create satisfactory logic
- - ❌ Create logic that sorts players into 3 even teams, based on player experience
- - ❌ The algorithm must not be based on magic or hard coded numbers or indexes
+ - ✅ Create logic that sorts players into 3 even teams, based on player experience
+ - ✅ The algorithm must not be based on magic or hard coded numbers or indexes
 
  #### Part 3: Personalized Letters
  - ❌ Store all 18 letters in the a collection called `letters`, including the player’s name, guardian names, team name, and date/time of their first team practice
  - ❌ Format each letter
  - ❌ Print each letter to the console
- - ❌ The word “Optional” must not be present in the letter content
+ - ❌ The word `Optional` must not be present in the letter content
 
  #### Part 4: Naming and Comments
  - ❌ Include comments
@@ -29,14 +29,13 @@
 
  ### Extra Credit
 
- - ❌ Logic must sort players into 3 even teams based on player experience *and* height
- - ❌ All teams must have an average height within 1.5 inches of each other
- - ❌ Must not depend on initial order of the collection of players
- - ❌ Print average height of each team to the console
+ - ✅ Logic must sort players into 3 even teams based on player experience *and* height
+ - ✅ All teams must have an average height within 1.5 inches of each other
+ - ✅ Must not depend on initial order of the collection of players
+ - ✅ Print average height of each team to the console
 
  ---
- ## Implementation
- ### Initial Definitions
+ ## Initial Definitions
 
  A player has a:
 
@@ -113,7 +112,7 @@ let teamPracticeTimeKey = "time"
     - `teamPracticeDateKey -> String`
     - `teamPracticeTimeKey -> String`
  - `teamPlayersKey -> [[String: Any]]`
-    - Each player has the structure discusses above between lines 62 and 67
+    - Each player has the structure discussed above between lines 62 and 67
 */
 // This would all be better as a `struct`, tuple, or at least a `typealias` so I'd only have to change 1 thing if I change the way I structure my data, which I've done like 4 times now
 // Also, I found an interesting Swift gotcha here. Apparently the type system doesn't like inferring `[String: Any]`, so you have to explicitly declare or cast it yourself
@@ -121,16 +120,49 @@ var teamSharks: [String: Any] = [teamNameKey: "Sharks", teamPracticeKey: [teamPr
 var teamDragons: [String: Any] = [teamNameKey: "Dragons", teamPracticeKey: [teamPracticeDateKey: "March 17", teamPracticeTimeKey: "1 PM"], teamPlayersKey: [[String: Any]]()]
 var teamRaptors: [String: Any] = [teamNameKey: "Raptors", teamPracticeKey: [teamPracticeDateKey: "March 18", teamPracticeTimeKey: "1 PM"], teamPlayersKey: [[String: Any]]()]
 
-//: I'm going to define a few helper functions I'm gonna use later. First up, in order to sort players normalizing the average heights of each team, I'm going to need to be able to get the average height of a particular team. I'm going to define 2 overloads here: the normal one that takes an array of players, and one for convenience that takes one of my team dictionaries
+/*:
+ ---
+ ## Sorting
+
+ Since the extra credit assignment is to basically sovle the [multi-way number partitioning problem](https://www.aaai.org/ocs/index.php/IJCAI/IJCAI-09/paper/viewFile/625/705), I'm going to be implementing a 3-way version of this problem's greedy heuristic in order to sort the players into their teams so that each team has an average height within 1.5 inches of each other, though my version of this algorithm will be slightly modified to account for capping the number of experienced players each team can have. This can be scaled to however many teams we decide there should be in the league, but we've predetermined 3 here
+
+ ### Ordering the `players`
+ The very first thing we need to do is sort the `players` array so it's ordered from the tallest player to the lowest player. Given that we're not allowed to use closures or most of the standard library yet, I'm going to make my own implementation of [Bubble Sort](https://en.wikipedia.org/wiki/Bubble_sort), which operates by iterating over a list 2 values at a time and swapping the values if they're not in the proper order, and reiterating until the array is sorted. It's not the most optimal sorting algorithm out there, but it's simple, and I've never seen a soccer team or league that has millions of players
+*/
+
+var hasSwapped = false
+
+repeat{
+    hasSwapped = false // Bubble sort knows it's done when it completes a full pass over the array without changing anything, so we need to start fresh after each pass
+    for index in 1..<players.count{ // Starting at the beginning of the array,
+        if let first = players[index - 1][playerHeightKey] as? Double, let second = players[index][playerHeightKey] as? Double, first < second{ // If an element earlier in the array is smaller than the one that comes after it,
+            (players[index - 1], players[index]) = (players[index], players[index - 1]) // Swap them
+            hasSwapped = true // Also, we need to remember the fact that we swapped things on this pass
+        }
+    }
+}while hasSwapped // As stated before, we want this to repeat until we've made a full pass without swapping anything
+
+//: As we can see, our `players` array is now properly sorted, with the tallest kid first and the shortest kid last 🎉
+
+players
+
+/*:
+ ### Constant, Variable, and Function Definitions
+ The gist of the greedy number partitioning heuristic is that we make an array for each group we want to sort into (3 in our case), and then we iterate through the `players`, and assign the next tallest player to the group that has the shortest total height. Given that there aren't any major outliers (which the project instructions say is something we can assume), this should come out to be pretty close to equal in height, most likely within 1.5 inches
+
+ However, it's important to note that there's also a limit to the number of experienced players a team is allowed to have.
+
+ In order to do this, we're gonna need some things that are either not in the standard library or I'm not allowed to use yet, so I'm gonna make a few helper functions. First up, in order to sort players normalizing the average heights of each team, I'm going to need to be able to get the total height of a particular team. I'm going to define 2 overloads here: the normal one that takes an array of players, and one for convenience that takes one of my team dictionaries. For what it's worth, this would usually be a perfect use case for `reduce` from the standard library, but that's one of the functions I'm not allowed to use here, so I'm just gonna roll my own
+*/
 
 /**
- Finds the average height of a list of players
+ Finds the total height of a list of players
 
  - parameter team: A list of players
- - returns: The average height of the players in `team`
+ - returns: The sum of the height of all players in `team`
  - precondition: Each player within the `team` must have a non-optional `Double` defined for `playerHeightKey`
-*/
-func averageHeight(of team: [[String: Any]]) -> Double{
+ */
+func totalHeight(of team: [[String: Any]]) -> Double{
     var total: Double = 0 // If I didn't mark this as a `Double` by default, it'd be inferred as an `Int` and I'd lose the decimals
     for player in team{
         guard let height = player[playerHeightKey] as? Double else{
@@ -143,39 +175,23 @@ func averageHeight(of team: [[String: Any]]) -> Double{
 }
 
 /**
- Finds the average height of a team
+ Finds the total height of a team
 
- This is a convenience overload for the primary `averageHeight(of:)`, which directly takes an `Array` of players. This finds that list within a team `Dictionary` and passes it to the original function
+ This is a convenience overload for the primary `totalHeight(of:)`, which directly takes an `Array` of players. This finds that list within a team `Dictionary` and passes it to the original function
 
  - parameter team: A `[String: Any]` representing a team
- - returns: The average height of the players in `team`
+ - returns: The sum of the height of all players in `team`
  - precondition: The `team` must have a non-optional `[String: Any]` defined for `teamPlayersKey`
  - precondition: Each player within the `team` must have a non-optional `Double` defined for `playerHeightKey`
-*/
-func averageHeight(of team: [String: Any]) -> Double{
+ */
+func totalHeight(of team: [String: Any]) -> Double{
     guard let players = team[teamPlayersKey] as? [[String: Any]] else{
         preconditionFailure("Couldn't find players list for team: \(team)")
     }
-    return averageHeight(of: players)
+    return totalHeight(of: players)
 }
 
-//: I'm gonna need to know whether or not a player has previous experience playing soccer. I know I'm already storing this as a simple `Bool` in a player's `Dictionary`, but I'm making a function for this so that I don't have to repeat all the unwrapping code all the time. I can't wait until I can use a `struct` so I can just mark this field non-`Optional` and call this a day 😛
-
-/**
- Finds if a player has previous soccer experience
-
- - parameter player: A `[String: Any]` representing a player
- - returns: A `Bool` indicating whether or not the `player` has previous soccer experience
- - precondition: The `player` must have a non-optional `Bool` defined for `playerExpreienceKey`
-*/
-func isExperienced(_ player: [String: Any]) -> Bool{
-    guard let experience = player[playerExperienceKey] as? Bool else{
-        preconditionFailure("Couldn't find experience record for player: \(player)")
-    }
-    return experience
-}
-
-//: Since we're not allowed to use `filter` from the standard library in this exercise, I'll have to make specialized versions myself when I need them. In this case, I'll need to know the experienced players on a given team. I'm gonna make 2 overloads for the same reason I did before with `averageHeight(of:)`
+//: Since we're also not allowed to use `filter` from the standard library in this exercise, I'll have to make specialized versions myself when I need them. In this case, I'll need to know the experienced players on a given team
 
 /**
  Finds all the players that have prior soccer experience in a list of players
@@ -183,57 +199,75 @@ func isExperienced(_ player: [String: Any]) -> Bool{
  - parameter team: An array representing a list of players
  - returns: An array of all players in `team` that have previous soccer experience
  - precondition: Each player in `team` must have a non-optional `Bool` defined for `playerExperienceKey`
-*/
+ */
 func experiencedPlayers(on team: [[String: Any]]) -> [[String: Any]]{ // Ideally I could `filter` here, but I wish I could at least use a generic
     var experienced = [[String: Any]]()
     for player in team{
-        if isExperienced(player){
+        if let hasExperience = player[playerExperienceKey] as? Bool, hasExperience{
             experienced.append(player)
         }
     }
     return experienced
 }
 
-/**
- Finds all players in a team that have prior soccer experience
+//: So, now that we have all the functions we'll need, let's get started on actually sorting the players. First, we're gonna need an `Array` that contains a `[[String: Any]]` to represent each team's list of players. It goes without saying that since we've decided on having 3 teams in this exercise, we'll need 3 of these `[[String: Any]]`s, though this can be easily changed for however many teams we want to have
 
- This is a convenience overload for the primary `experiencedPlayers(on:)`, which directly takes an `Array` of players. This finds that list within a team `Dictionary` and passes it to the original function
+var playerGroups = Array(repeating: [[String: Any]](), count: 3)
 
- - parameter team: A `[String: Any]` representing a team
- - returns: An array of all players in `team` that have previous soccer experience
- - precondition: The `team` must have a non-optional `[String: Any]` defined for `teamPlayersKey`
- - precondition: Each player in `team` must have a non-optional `Bool` defined for `playerExperienceKey`
+//: We'll also need to hang onto how many players a team is allowed to have. In this case, since there are only 3 teams, each teams will contain ⅓ of all players in the league
+
+let playerCap = Int((Double(players.count) / Double(playerGroups.count)).rounded()) // We have to do all this weird casting and rounding so players don't get left out if the league contains a number of players that aren't evenly divisible among the teams in the league. It basically just divides while accounting for decimals, rounding the result to the nearest ingeter, and converting it from a `Double` to an `Int`
+
+//: The last major constant we need to know ahead of time is the maximum number of experienced players each team is allowed to have. The easiest thing to do is to set it to simply be the total number of experienced players divided by the total number of teams, though this could cause problems with our current algorithm if that doesn't divide evenly. Given the data set for this project, though, it'll work just fine here. If the data set changes at some point, however, this is something that will need to be considered
+
+let experienceCap = experiencedPlayers(on: players).count / playerGroups.count
+
+/*:
+ ### Player Partitioning
+
+ Now that we have all the functions we need and have defined all the constants and variables we'll need, we can get down to actually sorting all the players into their teams. We're gonna do this by iterating over all the players in the league, determining if they have prior experience, finding the team that currently has the lowest total height, and placing the player onto that team. If that team happens to have reached its maximum number of players or maximum number of experienced players, we'll move on and keep looking for an eligible team with the next lowest total height
 */
-func experiencedPlayers(on team: [String: Any]) -> [[String: Any]]{
-    guard let players = team[teamPlayersKey] as? [[String: Any]] else{
-        preconditionFailure("Couldn't find players list for team: \(team)")
+
+for player in players{
+    //: The first thing we need to do on each iteration is to determine which team the next player will be sorted into. In most cases, this will simply be the one that currently has the lowest total height. However, if the next player to be sorted has previous soccer experience, we need to make sure that the team with the lowest total height hasn't also reached their cap of experienced players. If they have, we move onto the next lowest total height that hasn't reached its cap
+    guard let playerHasExperience = player[playerExperienceKey] as? Bool else{
+        preconditionFailure("Couldn't determine experience of player: \(player)") // Crashing is bad in production, but this isn't production, and it's a sign of bad data here that I would need to fix manually
     }
-    return experiencedPlayers(on: players)
+    var lowest = (height: Double.infinity, index: 0) // Usually I'd initialize the height here to 0, but that'd sorta break the whole "check if the current team is lower than the known lowest" thing, so I'm gonna initialize it to as high as possible
+    for (index, team) in playerGroups.enumerated(){ // This is just like enumerating over `playerGroups` directly, but we also get the index of the item we're on in the `playerGroups` array
+        let currentHeight = totalHeight(of: team)
+        guard currentHeight < lowest.height, team.count < playerCap, !playerHasExperience || experiencedPlayers(on: team).count < experienceCap else {
+            continue
+        }
+        lowest.height = currentHeight
+        lowest.index = index
+    }
+
+    playerGroups[lowest.index].append(player)
 }
+
+//: Once all the player lists are properly sorted, we just need to assign them to a preconstructed team
+
+teamSharks[teamPlayersKey] = playerGroups[0]
+teamDragons[teamPlayersKey] = playerGroups[1]
+teamRaptors[teamPlayersKey] =  playerGroups[2]
+
+//: Also, here's a quick definition for the rest of this playground. It's going to make everything from now on easier if I have an array of all the teams in the league
+
+let teams = [teamSharks, teamDragons, teamRaptors]
+
+//: Now we can see that all the teams have a list of players assigned to them, and they're all evenly matched, including having the same number of players, the same number of *experienced* players, and an average height within 1.5 inches of each other 🎉
+
+print("Team average heights:\n")
+for team in teams{
+    guard let name = team[teamNameKey] as? String, let members = team[teamPlayersKey] as? [[String: Any]] else{ // In practice, these will never fail, but you can't write code that's *too* safe
+        continue
+    }
+    print("- \(name): \(totalHeight(of: members) / Double(members.count)) inches")
+}
+print("\n---\n")
 
 /*:
  ---
- ### Sorting
-
- Since the extra credit assignment is to basically sovle the [multi-way number partitioning problem](https://www.aaai.org/ocs/index.php/IJCAI/IJCAI-09/paper/viewFile/625/705), I'm going to be implementing a 3-way version of this problem's greedy heuristic in order to sort the players into their teams so that each team has an average height within 1.5 inches of each other, though my version of this algorithm will be slightly modified to account for capping the number of experienced players each team can have. This can be scaled to however many teams we decide there should be in the league, but we've predetermined 3 here
-
- The very first thing we need to do is sort the `players` array so it's ordered from the tallest player to the lowest player. Given that we're not allowed to use closures or most of the standard library yet, I'm going to make my own implementation of [Bubble Sort](https://en.wikipedia.org/wiki/Bubble_sort), which operates by iterating over a list 2 values at a time and swapping the values if they're not in the proper order, and reiterating until the array is sorted. It's not the most optimal sorting algorithm out there, but it's simple, and I've never seen a soccer team or league that has millions of players
+ ## Letters
 */
-
-var hasSwapped = false
-
-players
-
-repeat{
-    hasSwapped = false // Bubble sort knows it's done when it completes a full pass over the array without changing anything, so we need to start fresh after each pass
-    for index in 1..<players.count{ // Starting at the beginning of the array,
-        if let first = players[index - 1][playerHeightKey] as? Double, let second = players[index][playerHeightKey] as? Double, first < second{ // If an element earlier in the array is smaller than the one that comes after it,
-            (players[index - 1], players[index]) = (players[index], players[index - 1]) // Swap them
-            hasSwapped = true
-        }
-    }
-}while hasSwapped
-
-//: As we can see, our `players` array is now properly sorted, with the tallest kid first and the shortest kid last 🎉
-
-players
